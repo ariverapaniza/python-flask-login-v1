14d1b0542e16bcc0ada85d2f82e628e367d92af7
Arturo Rivera <ariverapaniza@gmail.com>
2021-08-01 19:22:36 -0500

    added comment to the main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from website import create_app  #  This will take the folder website and use it as an python application 
 
-app = create_app()
+app = create_app()  # This will take the folder website and call the function create_app from the file __init__.py
 
 if __name__ == '__main__':  # This line will trigger the main application in this case Flask app. In the upper right corner there is a play button, which will run the web server with this code and the code written in the __init__.py file.
     app.run(debug=True)
